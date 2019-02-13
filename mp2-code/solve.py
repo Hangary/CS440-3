d92db6e29ec51a2033de4c60101aeb6aca66e079
houyuan2 <houyuan2@illinois.edu>
2019-02-13 01:27:53 -0600

    working on heuristic

--- mp2-code/solve.py
+++ mp2-code/solve.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 def solve(board, pents):
     """
     This is the function you will implement. It will take in a numpy array of the board
@@ -14,5 +13,39 @@ def solve(board, pents):
     
     -You can assume there will always be a solution.
     """
-    
-    raise NotImplementedError
+
+    initial = state(board, pents)
+    stack = [initial]
+    visited = dict()
+    while stack:
+        cur = stack.pop()
+        if hash(cur) in visited.keys():
+            continue
+        visited[hash(cur)] = 1
+
+        board = cur.board
+        pents = cur.pents
+
+        if (len(pents) == 0):
+            return board
+        
+    return []
+
+def check_placement(board, pent, coord):
+    for row in range(pent.shape[0]):
+        for col in range(pent.shape[1]):
+            if pent[row][col] != 0:
+                if coord[0]+row >= board.shape[0] or coord[1]+col >= board.shape[1]: # outside board
+                    return False
+                if board[coord[0]+row][coord[1]+col] != 0: # Overlap
+                    return False
+    return True
+
+class state():
+    def __init__(self, board, pents):
+        self.board = board
+        self.pents = pents
+
+    def __hash__(self):
+        self.hash = hash((self.board, self.pents))
+        return self.hash
