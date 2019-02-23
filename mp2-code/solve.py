fa542fecd93eaeca4ec798b7926a87d2e9271a56
houyuan2 <houyuan2@illinois.edu>
2019-02-22 18:23:35 -0600

    clean up

--- mp2-code/solve.py
+++ mp2-code/solve.py
@@ -28,19 +28,25 @@ def solve(board, pents, app = None):
             coor_remain.add(coordinate)
     
     pents_remain = []
+    all_pents = dict()
     for p in pents:
-        pents_remain.append(get_pent_idx(p))
+        idx = get_pent_idx(p)
+        pents_remain.append(idx)
+
+        rot_flip = generate_ori(p)
+        all_pents[idx] = rot_flip
     
     for y in range(board.shape[0]):
         for x in range(board.shape[1]):
             coordinate = (y,x)
             for pent in pents:
-                rot_flip_list = generate_ori(pent)
+                pidx = get_pent_idx(pent)
+                rot_flip_list = all_pents[pidx]
                 for ori_pent in rot_flip_list:
                     cor_add_list =  check_placement(board, ori_pent, coordinate)
                     if cor_add_list != None:
                         for coor in cor_add_list:
-                            cor_pent_dict[coor].append((coordinate, ori_pent, cor_add_list))
+                            cor_pent_dict[coor].append((coordinate, ori_pent, cor_add_list, pidx))
 
     solution = recursion([], cor_pent_dict, coor_remain, pents_remain)
     # print(solution)
@@ -57,8 +63,8 @@ def recursion(solution, cor_pent_dict, coor_remain, pents_remain):
         assignment_cor = value[0]
         ori_pent = value[1]
         cor_add_list = value[2]
+        pidx = value[3]
 
-        pidx = get_pent_idx(ori_pent)
         solution.append((ori_pent, assignment_cor))
         pents_remain.remove(pidx)
                 
@@ -75,7 +81,7 @@ def recursion(solution, cor_pent_dict, coor_remain, pents_remain):
         for c in cor_pent_dict.keys():
             for i in range(len(cor_pent_dict[c])- 1, -1, -1):
                 temp = cor_pent_dict[c][i]
-                idx = get_pent_idx(temp[1])
+                idx = temp[3]
                 temp_set = temp[2]
                 if idx == pidx or len(temp_set & cor_add_list) != 0:
                     temp_pop_list.append((c, i, temp))
