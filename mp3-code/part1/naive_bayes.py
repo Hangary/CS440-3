33216224a1dfe5b1e2f2e2be6fe1b0f564d4eb18
Haiming Zhang <haiming2@illinois.edu>
2019-03-23 01:53:55 -0500

    a

--- mp3-code/part1/naive_bayes.py
+++ mp3-code/part1/naive_bayes.py
@@ -1,106 +1,109 @@
 import numpy as np
 
-
 class NaiveBayes(object):
-    def __init__(self, num_class, feature_dim, num_value):
+    def __init__(self,num_class,feature_dim,num_value):
         """Initialize a naive bayes model.
-
-        This function will initialize prior and likelihood, where
-        prior is P(class) with a dimension of (# of class,)
-            that estimates the empirical frequencies of different classes in the training set.
-        likelihood is P(F_i = f | class) with a dimension of
-            (# of features/pixels per image, # of possible values per pixel, # of class),
-            that computes the probability of every pixel location i being value f for every class label.
-
-        Args:
-            num_class(int): number of classes to classify
-            feature_dim(int): feature dimension for each example
-            num_value(int): number of possible values for each pixel
-        """
+		This function will initialize prior and likelihood, where
+		prior is P(class) with a dimension of (# of class,)
+			that estimates the empirical frequencies of different classes in the training set.
+		likelihood is P(F_i = f | class) with a dimension of
+			(# of features/pixels per image, # of possible values per pixel, # of class),
+			that computes the probability of every pixel location i being value f for every class label.
+		Args:
+		    num_class(int): number of classes to classify
+		    feature_dim(int): feature dimension for each example
+		    num_value(int): number of possible values for each pixel
+		"""
 
         self.num_value = num_value
         self.num_class = num_class
         self.feature_dim = feature_dim
 
         self.prior = np.zeros((num_class))
-        self.likelihood = np.zeros((feature_dim, num_value, num_class))
-        self.k = 1
+        self.likelihood = np.zeros((feature_dim,num_value,num_class))
 
-    def train(self, train_set, train_label):
+    def train(self,train_set,train_label):
         """ Train naive bayes model (self.prior and self.likelihood) with training dataset.
-            self.prior(numpy.ndarray): training set class prior (in log) with a dimension of (# of class,),
-            self.likelihood(numpy.ndarray): traing set likelihood (in log) with a dimension of
-                (# of features/pixels per image, # of possible values per pixel, # of class).
-            You should apply Laplace smoothing to compute the likelihood.
-
-        Args:
-            train_set(numpy.ndarray): training examples with a dimension of (# of examples, feature_dim)
-            train_label(numpy.ndarray): training labels with a dimension of (# of examples, )
-        """
+			self.prior(numpy.ndarray): training set class prior (in log) with a dimension of (# of class,),
+			self.likelihood(numpy.ndarray): traing set likelihood (in log) with a dimension of
+				(# of features/pixels per image, # of possible values per pixel, # of class).
+			You should apply Laplace smoothing to compute the likelihood.
+		Args:
+		    train_set(numpy.ndarray): training examples with a dimension of (# of examples, feature_dim)
+		    train_label(numpy.ndarray): training labels with a dimension of (# of examples, )
+		"""
         # YOUR CODE HERE
-        print("train start")
+        K = 1
+        num_class = self.num_class
+        num_examples = train_set.shape[0]
 
-        for label in range(self.num_class):
-            self.prior[label] = np.sum(train_label == label) / len(train_label)
-        counter = np.zeros((self.feature_dim, self.num_value, self.num_class)) + self.k
+        feature_dim = self.feature_dim
+        prior = self.prior
+        likelihood = self.likelihood
 
-        num_train_example = train_set.shape[0]
-        for idx in range(num_train_example):
-            label = train_label[idx]
-            for i, feat in enumerate(train_set[idx, :]):
-                counter[i, feat, label] += 1
-        for label in range(self.num_class):
-            for feature in range(self.feature_dim):
-                self.likelihood[feature, :, label] = counter[feature, :, label] / np.sum(counter[feature, :, label])
+        for n in range(num_examples):
+            ex_feature = train_set[n]
+            ex_label = train_label[n]
 
-        print("train finish")
+            prior[ex_label] += 1
 
+            for f in range(feature_dim):
+                likelihood[f][ex_feature[f]][ex_label] += 1
 
+        likelihood += K
+        for c in range(num_class):
+             likelihood[:,:,c] = likelihood[:,:,c] / (prior[c] + K * 256)
 
-    def test(self, test_set, test_label):
+        prior  = prior / num_examples
+
+    def test(self,test_set,test_label):
         """ Test the trained naive bayes model (self.prior and self.likelihood) on testing dataset,
-            by performing maximum a posteriori (MAP) classification.
-            The accuracy is computed as the average of correctness
-            by comparing between predicted label and true label.
+			by performing maximum a posteriori (MAP) classification.
+			The accuracy is computed as the average of correctness
+			by comparing between predicted label and true label.
+		Args:
+		    test_set(numpy.ndarray): testing examples with a dimension of (# of examples, feature_dim)
+		    test_label(numpy.ndarray): testing labels with a dimension of (# of examples, )
+		Returns:
+			accuracy(float): average accuracy value
+			pred_label(numpy.ndarray): predicted labels with a dimension of (# of examples, )
+		"""
 
-        Args:
-            test_set(numpy.ndarray): testing examples with a dimension of (# of examples, feature_dim)
-            test_label(numpy.ndarray): testing labels with a dimension of (# of examples, )
+		# YOUR CODE HERE
 
-        Returns:
-            accuracy(float): average accuracy value
-            pred_label(numpy.ndarray): predicted labels with a dimension of (# of examples, )
-        """
-
-        # YOUR CODE HERE
-
-        # accuracy = 0
-        print("test start")
+        accuracy = 0
         pred_label = np.zeros((len(test_set)))
-        for idx in range(len(test_set)):
-            pred_label[idx] = self.predict(test_set[idx, :])
-        accuracy = np.sum(pred_label == test_label) / len(test_label)
 
-        print("test finish")
+        num_class = self.num_class
+        prior = self.prior
+        likelihood = self.likelihood
+        feature_dim = self.feature_dim
+
+        for t in range(len(test_set)):
+            ex_feature = test_set[t]
+            possibities = np.zeros(len(prior))
+            possibities += np.log(prior)
+            for i, v in enumerate(ex_feature):
+                possibities += np.log(likelihood[i][v])
+
+            pred_label[t] = np.argmax(possibities)
+
+        accuracy = np.sum(pred_label == test_label) / len(test_set)
+
+        print("test complete")
         return accuracy, pred_label
 
-    def predict(self, test):
-        log_probs = np.log(self.prior)
-        for label in range(self.num_class):
-            for feature in range(self.feature_dim):
-                log_probs[label] += np.log(self.likelihood[feature, test[feature], label])
-        return np.argmax(log_probs)
 
     def save_model(self, prior, likelihood):
         """ Save the trained model parameters
-        """
+		"""
 
         np.save(prior, self.prior)
         np.save(likelihood, self.likelihood)
 
     def load_model(self, prior, likelihood):
         """ Load the trained model parameters
-        """
+		"""
 
         self.prior = np.load(prior)
         self.likelihood = np.load(likelihood)
@@ -111,7 +114,6 @@ class NaiveBayes(object):
             by sum the probabilities of the top 128 intensities at each pixel location,
             sum k<-128:255 P(F_i = k | c).
             This helps generate visualization of trained likelihood images.
-
         Args:
             likelihood(numpy.ndarray): likelihood (in log) with a dimension of
                 (# of features/pixels per image, # of possible values per pixel, # of class)
@@ -119,13 +121,13 @@ class NaiveBayes(object):
             feature_likelihoods(numpy.ndarray): feature likelihoods for each class with a dimension of
                 (# of features/pixels per image, # of class)
         """
-        # YOUR CODE HERE
+	    # YOUR CODE HERE
+        likelihood = self.likelihood
+        feature_likelihoods = np.zeros((likelihood.shape[0],likelihood.shape[2]))
 
-        feature_likelihoods = np.zeros((likelihood.shape[0], likelihood.shape[2]))
-        for feature in range(self.feature_dim):
-            for label in range(self.num_class):
-                vals_prob = likelihood[feature, :, label]
-                prob_sum = np.argsort(vals_prob)[-128:]
-                feature_likelihoods[feature][label] = np.sum(vals_prob[prob_sum])
+        for d in range(likelihood.shape[0]):
+            for c in range(likelihood.shape[2]):
+                vals_prob = likelihood[d, :, c]
+                prob_sort = np.argsort(vals_prob)
+                feature_likelihoods[d][c] = np.sum(vals_prob[prob_sort][128:])
         return feature_likelihoods
-
--- mp3-code/part1/perceptron.py
+++ mp3-code/part1/perceptron.py
@@ -2,25 +2,27 @@ import numpy as np
 
 class MultiClassPerceptron(object):
 	def __init__(self,num_class,feature_dim):
-		"""Initialize a multi class perceptron model. 
+		"""Initialize a multi class perceptron model.
 
 		This function will initialize a feature_dim weight vector,
-		for each class. 
+		for each class.
 
 		The LAST index of feature_dim is assumed to be the bias term,
-			self.w[:,0] = [w1,w2,w3...,BIAS] 
+			self.w[:,0] = [w1,w2,w3...,BIAS]
 			where wi corresponds to each feature dimension,
-			0 corresponds to class 0.  
+			0 corresponds to class 0.
 
 		Args:
 		    num_class(int): number of classes to classify
-		    feature_dim(int): feature dimension for each example 
+		    feature_dim(int): feature dimension for each example
 		"""
 
 		self.w = np.zeros((feature_dim+1,num_class))
+		self.b = 1
+		self.learning_rate = 0.8
 
 	def train(self,train_set,train_label):
-		""" Train perceptron model (self.w) with training dataset. 
+		""" Train perceptron model (self.w) with training dataset.
 
 		Args:
 		    train_set(numpy.ndarray): training examples with a dimension of (# of examples, feature_dim)
@@ -28,39 +30,55 @@ class MultiClassPerceptron(object):
 		"""
 
 		# YOUR CODE HERE
-		pass
+		num_train = train_set.shape[0]
+		train_set_b = np.append(train_set, np.ones((num_train,1)), axis=1)
+		epoch = 100
+		decay = self.learning_rate/epoch
+		for iteration in range(epoch):
+			predict_matrix = np.matmul(train_set_b,self.w)
+			for i in range(num_train):
+				f = train_set_b[i,:]
+				#predict_label = np.argmax(np.matmul(f,self.w))
+				predict_label = np.argmax(predict_matrix[i,:])
+				true_label = train_label[i]
+				if predict_label != true_label:
+					self.w[:,true_label] += self.learning_rate * f
+					self.w[:,predict_label] -= self.learning_rate * f
+			self.learning_rate *= 0.95
+
 
 	def test(self,test_set,test_label):
-		""" Test the trained perceptron model (self.w) using testing dataset. 
-			The accuracy is computed as the average of correctness 
-			by comparing between predicted label and true label. 
-			
+		""" Test the trained perceptron model (self.w) using testing dataset.
+			The accuracy is computed as the average of correctness
+			by comparing between predicted label and true label.
+
 		Args:
 		    test_set(numpy.ndarray): testing examples with a dimension of (# of examples, feature_dim)
 		    test_label(numpy.ndarray): testing labels with a dimension of (# of examples, )
 
 		Returns:
-			accuracy(float): average accuracy value 
+			accuracy(float): average accuracy value
 			pred_label(numpy.ndarray): predicted labels with a dimension of (# of examples, )
-		"""    
+		"""
 
 		# YOUR CODE HERE
-		accuracy = 0 
+		accuracy = 0
 		pred_label = np.zeros((len(test_set)))
+		test_set_b = np.append(test_set, np.ones((test_set.shape[0],1)), axis=1)
+		scores = np.matmul(test_set_b,self.w)
+		pred_label = np.argmax(scores, axis=1)
+		accuracy = np.sum(pred_label == test_label) / len(test_set)
 
-		pass
-		
 		return accuracy, pred_label
 
 	def save_model(self, weight_file):
-		""" Save the trained model parameters 
-		""" 
+		""" Save the trained model parameters
+		"""
 
 		np.save(weight_file,self.w)
 
 	def load_model(self, weight_file):
-		""" Load the trained model parameters 
-		""" 
+		""" Load the trained model parameters
+		"""
 
 		self.w = np.load(weight_file)
-
