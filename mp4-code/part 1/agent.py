4957cffbd2d1a70f6ccf1120bb45b2e4d94b4713
houyuan2 <houyuan2@illinois.edu>
2019-04-10 18:49:57 -0500

    finish training

--- mp4-code/part 1/agent.py
+++ mp4-code/part 1/agent.py
@@ -15,6 +15,11 @@ class Agent:
         self.Q = utils.create_q_table()
         self.N = utils.create_q_table()
 
+        #last state
+        self.s
+        #last action
+        self.a
+
     def train(self):
         self._train = True
         
@@ -47,5 +52,80 @@ class Agent:
         (Note that [adjoining_wall_x=0, adjoining_wall_y=0] is also the case when snake runs out of the 480x480 board)
 
         '''
+        # training
+        if self.train:
+            # updating
+            cur_s = state_index(state)
+            last_Q_value = self.Q[self.s][self.a]
+            update_value = last_Q_value + alpha(self) * (reward(self, dead) + self.gamma * max(self.Q[cur_s])  - last_Q_value)
+            self.Q[self.s][self.a] = update_value # update Q-table!
 
-        return self.actions[0]
+            # action
+            max_v = -float("inf")
+            cur_a = 0
+            for i in range (3, -1, -1):
+                if self.N[cur_s][i] < self.Ne:
+                    cur_a = i
+                    break
+
+                if self.Q[cur_s][i] > max_v:
+                    max_v = v
+                    cur_a = i 
+            self.s = cur_s
+            self.a = cur_a       
+        # testing
+        else:
+    
+    def alpha(self):
+        return self.C/(self.C+self.N[self.s][self.a])
+
+    def reward(self, dead):
+        if dead:
+            return -1
+        elif self.state[0] == self.state[3] and self.state[1] == self.state[4]:  # snake eats food
+            return 1
+        else:
+            return -0.1
+
+    def state_index(state):
+        adjoining_wall_x = 0
+        adjoining_wall_y = 0
+        food_dir_x = 0
+        food_dir_y = 0
+        adjoining_body_top = 0
+        adjoining_body_bottom = 0
+        adjoining_body_left = 0
+        adjoining_body_right = 0
+
+        if state.snake_head_x == 40:
+            adjoining_wall_x = 1
+        elif state.snake_head_x == 520:
+            adjoining_wall_x = 2
+        
+        if state.snake_head_y == 40:
+            adjoining_wall_y = 1
+        elif state.snake_head_y == 520:
+            adjoining_wall_y = 2
+        
+        if state.food_x < state.snake_head_x:
+            food_dir_x = 1
+        elif state.food_x > state.snake_head_x:
+            food_dir_x = 2
+
+        if state.food_y < state.snake_head_y:
+            food_dir_y = 1
+        elif state.food_y > state.snake_head_y:
+            food_dir_y = 2
+
+        for x,y in state.snake_body:
+            if state.snake_head_x == x + 40:
+                adjoining_body_left = 1
+            if state.snake_head_x == x - 40:
+                adjoining_body_right = 1
+            if state.snake_head_y == y - 40:
+                adjoining_body_top = 1
+            if state.snake_head_y == y + 40:
+                adjoining_body_bottom = 1
+        
+        return (adjoining_wall_x, adjoining_wall_y, food_dir_x, food_dir_y, adjoining_body_top, 
+        adjoining_body_bottom, adjoining_body_left, adjoining_body_right)
